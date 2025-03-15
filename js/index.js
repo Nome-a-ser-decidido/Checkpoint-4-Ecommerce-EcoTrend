3ff51b84d05fe3c9deae26af17d2d82cacdb1fb8
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-03-14 23:28:22 -0300

    Criando o headers do site

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.querySelector('.cart-image').src = './images/shopping-cart-hover.png';
+}
+
+function unhover(element) {
+    element.querySelector('.cart-image').src = './images/shopping-cart.png';
+}
